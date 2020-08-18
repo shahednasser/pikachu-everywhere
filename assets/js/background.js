06bdb861b3e546cf0e2773c84367404b93ae1a76
Shahed Nasser <shahednasser@gmail.com>
2020-08-18 09:25:24 +0300

    Updated API url

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
   if(message.msg === "image"){
-    fetch('https://some-random-api.ml/pikachuimg')
+    fetch('https://some-random-api.ml/img/pikachu')
           .then(response => response.text())
           .then(data => {
             let dataObj = JSON.parse(data);
